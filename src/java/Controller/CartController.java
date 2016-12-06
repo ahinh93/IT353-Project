ff8457ec7d6c879119d0aa05a7ceaeb5a2549918
it353F608 <it353F608@ad.ilstu.edu>
2016-12-06 09:22:41 -0600

    adding checkouts and email receipts

--- src/java/Controller/CartController.java
+++ src/java/Controller/CartController.java
@@ -7,10 +7,15 @@ package Controller;
 
 import dao.CartDAO;
 import dao.CartDAOImpl;
+import dao.DBHelper;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import model.Cart;
 
 /**
@@ -23,17 +28,9 @@ public class CartController {
 
     @ManagedProperty("#{loginController}")
     private LoginController lc;
-
-    public LoginController getLc() {
-        return lc;
-    }
-
-    public void setLc(LoginController lc) {
-        this.lc = lc;
-    }
-    private ArrayList<Cart> myCart;
-    
+    private ArrayList<Cart> myCart;    
     CartDAOImpl myDAO = new CartDAOImpl();
+    
     /**
      * Creates a new instance of ShoppingController
      */
@@ -42,18 +39,92 @@ public class CartController {
         myCart = new ArrayList<Cart>();
     }
     
-    public int addToCart(int mediaID)
-    {
-        System.out.println("mediaID: " + mediaID);
-        return mediaID;
+    public LoginController getLc() {
+        return lc;
     }
     
-    
-    public void checkout()
+    public int addToCart()
     {
+        String mediaID = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("mediaIDkey");
+        System.out.println("Adding media: " + mediaID +" to the cart table");
+        return 1;
+    }    
+    /**
+     * TODO:
+     * 1.insert all entries with that email into a string array and double array
+     * 2.pass arrays into email controller --> emailReceipt(media_id,price)
+     * 3.delete entries from database
+     * @return 
+     */
+    public String checkout()
+    {
+        System.out.println("@@@@@@@@@@"+lc.getEmail()+" just hit the checkout button");
+        prepareReceipt();
         
-    }
+        DBHelper.loadDriver("org.apache.derby.jdbc.ClientDriver");
+        String myDB = "jdbc:derby://localhost:1527/it353finalproject";
+        //DELETE FROM CART WHERE email = 'ahinh@ilstu.edu' and media_id = 202;
+        String query = "DELETE FROM CART WHERE EMAIL = ?";
+        Connection DBConn = DBHelper.connect2DB(myDB, "admin1", "password");
 
+        try{
+      
+            PreparedStatement preparedStmt = DBConn.prepareStatement(query);
+
+            preparedStmt.setString (1, lc.getEmail());
+            // execute the preparedstatement
+            preparedStmt.execute();
+            DBConn.close();
+
+
+            //send receipt to purchaser
+            EmailController ec = new EmailController();
+
+            //send royalty to artist
+
+            } catch (Exception e) {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+            return "failedpurchase.xhtml";
+        }
+        return "successfulcheckout.xhtml";
+    }
+    public boolean prepareReceipt()
+    {
+        System.out.println("@@@@@@@@@@"+lc.getEmail()+" just hit the checkout button");
+        prepareReceipt();
+        
+        DBHelper.loadDriver("org.apache.derby.jdbc.ClientDriver");
+        String myDB = "jdbc:derby://localhost:1527/it353finalproject";
+        //DELETE FROM CART WHERE email = 'ahinh@ilstu.edu' and media_id = 202;
+        String query = "DELETE FROM CART WHERE EMAIL = ?";
+        Connection DBConn = DBHelper.connect2DB(myDB, "admin1", "password");
+
+        try{
+      
+            PreparedStatement preparedStmt = DBConn.prepareStatement(query);
+
+            preparedStmt.setString (1, lc.getEmail());
+            // execute the preparedstatement
+            preparedStmt.execute();
+            DBConn.close();
+
+
+            //send receipt to purchaser
+            EmailController ec = new EmailController();
+
+            //send royalty to artist
+
+            } catch (Exception e) {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+    public void setLc(LoginController lc) {
+        this.lc = lc;
+    }
     /**
      * @return the myCart
      */
@@ -69,10 +140,6 @@ public class CartController {
      */
     public void setMyCart(ArrayList<Cart> myCart) {
         this.myCart = myCart;
-    }
+    }     
     
-    
-    public void addToCart(){
-        System.out.println("here");
-    }    
 }
--- src/java/Controller/EmailController.java
+++ src/java/Controller/EmailController.java
@@ -93,4 +93,121 @@ public class EmailController {
         }
         return "failure";
     }
+    public void emailReceipt(String email,int[] media_id, double[] price){
+
+        // Recipient's email ID needs to be mentioned.
+        String to = email;
+
+        // Sender's email ID needs to be mentioned
+        String from = "ahinh@ilstu.edu";
+        
+        // Assuming you are sending email from this host
+        String host = "outlook.office365.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.port", "587");
+        // Get the default Session object.
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("ahinh@ilstu.edu", "46mr4bpbdaY");
+            }
+        });
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("Purchase Receipt - Payment Confirmation");
+            
+            String out = "Artwork purchased: <br><ul>";
+            double totalCost = 0;
+            for(int i = 0; i < media_id.length; i++)
+            {
+                out+="<li>" +media_id[i]+" " + price[i] + "</ul>";
+                totalCost += price[i];
+            }
+            out+="</ul>";
+            
+            // Send the actual HTML message, as big as you like
+            message.setContent("<h1>Thank you, "+email+" for supporting your local artists!.</h1>"
+                    + "<p>"+out+"<br>"
+                    + "Total cost: $"+String.format("%.2f", totalCost)+".</p>",
+                    "text/html");
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+    public void emailRoyalty(String email,double amount){
+
+        // Recipient's email ID needs to be mentioned.
+        String to = email;
+
+        // Sender's email ID needs to be mentioned
+        String from = "ahinh@ilstu.edu";
+        
+        // Assuming you are sending email from this host
+        String host = "outlook.office365.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.port", "587");
+        // Get the default Session object.
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("ahinh@ilstu.edu", "46mr4bpbdaY");
+            }
+        });
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("Royalty Payment Received!");
+            
+            
+            
+            
+            // Send the actual HTML message, as big as you like
+            message.setContent("<h1>You have received payment for one of your artworks!</h1>"
+                    + "<p>Enjoy the amount of $"+String.format("%.2f", amount)+".</p>",
+                    "text/html");
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
 }
